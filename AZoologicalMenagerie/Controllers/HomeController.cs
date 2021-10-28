4f157f355b1b5a12d15f192110a14c7ea538935b
Samuel Greenlee <sg630912@southeast.edu>
2021-10-28 10:22:41 -0500

    Set up project

--- AZoologicalMenagerie/Controllers/HomeController.cs
+++ AZoologicalMenagerie/Controllers/HomeController.cs
@@ -17,14 +17,63 @@ namespace AZoologicalMenagerie.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Welcome to the best Zoo in town.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Got a question, send it to the email below.";
+
+            return View();
+        }
+
+        public IActionResult Bears()
+        {
+            ViewData["Message"] = "Welcome to the home of the bears";
+
+            return View();
+        }
+
+        public IActionResult Wolves()
+        {
+            ViewData["Message"] = "Welcome to the home of the wolves";
+
+            return View();
+        }
+
+        public IActionResult Foxes()
+        {
+            ViewData["Message"] = "Welcome to the home of the foxes";
+
+            return View();
+        }
+
+        public IActionResult Turtles()
+        {
+            ViewData["Message"] = "Welcome to the home of the turtles";
+
+            return View();
+        }
+
+        public IActionResult Fish()
+        {
+            ViewData["Message"] = "With hundreds of fish, here are some species to look out for in our pond.";
+
+            return View();
+        }
+
+        public IActionResult Merchandise()
+        {
+            ViewData["Message"] = "Feel free to look for what you need.";
+
+            return View();
+        }
+
+        public IActionResult EnclosuresPending()
+        {
+            ViewData["Message"] = "Featuring, the pending enclosures.";
 
             return View();
         }
